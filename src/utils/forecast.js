47e11220b1e40053970392e42bcfed0807ccb921
sjbenenate <sarahjoybenenate@gmail.com>
2019-10-30 07:14:12 -0700

    added time for low and high

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,7 +6,7 @@ const forecast = (latitude, longitude, callback) => {
     const darkSkyAPIKey = fs.readFileSync('./src/keys/darkSkyAPIKey.txt')
     const darkSkyQueryOptions = '?lang=en&units=us&exclude=minutely,hourly,alerts,flags'
     const url = 'https://api.darksky.net/forecast/' + darkSkyAPIKey + '/' + latitude + ',' + longitude + darkSkyQueryOptions
-    //console.log(url)
+    console.log(url)
 
     request({url, json: true}, (error, {body: forecastResponse} = {}) => {
         if (error) {
@@ -16,13 +16,18 @@ const forecast = (latitude, longitude, callback) => {
             callback('Error in weather forcast response', {undefined})
         }
         else {
-            dailyHigh = Math.round(forecastResponse.daily.data[0].temperatureHigh),  
-            dailyLow = Math.round(forecastResponse.daily.data[0].temperatureLow),
-            currentTemp = Math.round(forecastResponse.currently.temperature),
+            dailyHigh = Math.round(forecastResponse.daily.data[0].temperatureHigh)
+
+            highTime = new Date(forecastResponse.daily.data[0].temperatureHighTime)
+            lowTime = new Date(forecastResponse.daily.data[0].temperatureLowTime)
+            highHours = highTime.getHours()
+            highMinutes = highTime.getMinutes()
+            dailyLow = Math.round(forecastResponse.daily.data[0].temperatureLow)
+            currentTemp = Math.round(forecastResponse.currently.temperature)
             currentPercip = Math.round(forecastResponse.currently.precipProbability)
             summary = forecastResponse.currently.summary
 
-            callback(undefined, summary + '. Currently ' + currentTemp + 'F with ' + currentPercip + '% percipitation. The high today is ' + dailyHigh + 'F with a low of ' + dailyLow + 'F.')
+            callback(undefined, summary + '. Currently ' + currentTemp + 'F. High today of ' + dailyHigh + 'F at ' + highTime + '. Low today of ' + dailyLow + 'F at ' + lowTime + '.')
         }
     })
    
